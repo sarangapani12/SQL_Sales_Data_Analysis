15dc198c47c1b802427c922610a2f825d67fffab
sarangapani12 <sarangapanir04@gmail.com>
2026-04-22 19:14:34 +0530

    Add table creation script

---
+++ Table_Creation.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS records;
+CREATE TABLE records
+            (
+                transaction_id INT PRIMARY KEY,	
+                sale_date DATE,	 
+                sale_time TIME,	
+                customer_id	INT,
+                gender	VARCHAR(15),
+                age	INT,
+                category VARCHAR(15),	
+                quantity	INT,
+                price_per_unit FLOAT,	
+                cogs	FLOAT,
+                total_sale FLOAT
+            );
+SELECT * FROM records 
+LIMIT 10;
